4afff169e8ef1cc8b45e81a4c2031467871fd834
mohakjo <mohameddioume@gmail.com>
2023-06-01 14:07:43 +0200

    Update databaseco.php

--- Stie Ecomerce/app/modem/databaseco.php
+++ Stie Ecomerce/app/modem/databaseco.php
@@ -2,27 +2,21 @@
 
 function getdbco(): PDO 
 {
-$serveur = "localhost";
-$dbname = "sqlite:C:\wamp\www\pokemon\app\database\pokemon.sqlite";
-$user = "root";
-$pass = "";
-    
+$dsn="mysql:host=localhost;dbname=cap;charset=utf8";
+$user="root";
+$pass="";
 
     try{
-        
+
         //On se connecte à la BDD
-        $dbco = new PDO($dbname);
+        $dbco=new PDO($dsn,$user,$pass);
         $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbco->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-
     }
     catch(PDOException $e){
         echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
     }
 return $dbco;
 }
-
-
 
 ?>
