2cdf6a86ee44cf1f2bfd10bc7ff1788307ee9c17
mohakjo <mohameddioume@gmail.com>
2023-06-01 15:27:14 +0200

    vava

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $css = 'index.css';
 
 
 ob_start();
-require_once 'app/view/index.view.php';
+require_once 'app/view/parts/index.view.php';
 $content = ob_get_clean();
 require_once 'app/view/common/layout.php';
 
